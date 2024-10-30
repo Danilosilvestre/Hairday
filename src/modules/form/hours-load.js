392cf30f25ebfe048558479fc7065ecafe240017
Danilo Da Cruz <danilo.s_95@outlook.com>
2024-10-30 14:24:58 -0300

    Enviando um horário selecionado para a API

--- src/modules/form/hours-load.js
+++ src/modules/form/hours-load.js
@@ -1,5 +1,6 @@
 import dayjs from "dayjs"
 import {openingHours} from "../../utils/opening-hours.js"
+import { hoursClick } from "./hours-click.js"
 
 const hours = document.getElementById("hours")
 
@@ -35,11 +36,15 @@ export function hoursLoad({date}){
 
   hours.append(li)
 })
+
+// Adiciona um evento de clique nos horários disponíveis.
+ hoursClick()
 }
 
 function hourHeaderAdd(title){
   const header = document.createElement("li")
   header.classList.add("hour-period")
   header.textContent = title
+
   hours.append(header)
 }
