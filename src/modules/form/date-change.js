c5b1c378461b06034880077c397e05db78ad4571
Danilo Da Cruz <danilo.s_95@outlook.com>
2024-11-04 09:40:52 -0300

    Finalizando o projeto

---
+++ src/modules/form/date-change.js
@@ -0,0 +1,7 @@
+import {schedulesDay} from "../schedules/load"
+//Seleciona o imput da data
+const selectedDate = document.getElementById("date")
+
+// Recarregar a lista de horários quando a data for alterada.
+selectedDate.onchange = () => schedulesDay ()
+
